7c210c358f6ce191bc64ad25e873161b6f174264
Agnieszka Lukasiak <lukasiak@me.com>
2024-07-14 11:54:37 +0000

    add winner function in js

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 /* declaring variables, linking to dom elements*/
 
-const playerObj= document.qetElementById("player");
-const computerObj= document.qetElementById("computer");
-const resultObj= document.qetElementById("result");
-const buttonObj= document.qetElementById("button");
-const computerScoreObj=document.qetElementById("scoreC");
-const playerScoreObj=document.qetElementById("scoreP");
+const playerObj = document.qetElementById("player");
+const computerObj = document.qetElementById("computer");
+const resultObj = document.qetElementById("result");
+const buttonObj = document.qetElementById("button");
+const computerScoreObj = document.qetElementById("scoreC");
+const playerScoreObj = document.qetElementById("scoreP");
 let player;
 let computer;
 let result;
@@ -15,31 +15,61 @@ let computerScore = 0;
 
 /* getting buttons to work */
 
-buttonObj. forEach (function buttonClick(button){button.addEventListener("click", buttonClick()) 
-    {player=button.textContent;
-    computerTurn();
-    playerObj.textContent = 'Player:${player}';
-    computerObj.textContent='Computer:${computer}';
-    resultObj.textContent = checkWinner();
-}});
+buttonObj.forEach(function buttonClick(button) {
+    button.addEventListener("click", buttonClick())
+    {
+        player = button.textContent;
+        computersTurn();
+        playerObj.textContent = 'Player:${player}';
+        computerObj.textContent = 'Computer:${computer}';
+        resultObj.textContent = checkWinner();
+    }
+});
 
-function computerTurn(){ const randNum = Math.floor(Math.random()*5)+1;
-    switch(randNum){
+/* computers function */
+
+function computersTurn() {
+    const randNum = Math.floor(Math.random() * 5) + 1;
+    switch (randNum) {
         case 1:
-            computer:'ROCK';
+            computer: 'ROCK';
             break;
-            case 2:
-            computer:'PAPER';
+        case 2:
+            computer: 'PAPER';
             break;
-            case 3:
-            computer:'SCISSORS';
+        case 3:
+            computer: 'SCISSORS';
             break;
-            case 4:
-            computer:'LIZARD';
+        case 4:
+            computer: 'LIZARD';
             break;
-            case 1:
-            computer:'SPOCK';
+        case 1:
+            computer: 'SPOCK';
             break;
     }
 }
-    
+
+/* winner function */
+
+function Winner() {
+    if (player == computer) {
+        return "Draw!";
+    }
+    else if (computer === "ROCK") {
+        return (player === "PAPER" || player === "SPOCK") ? "You win!" : "You lose!";
+    }
+    else if (computer === "PAPER") {
+        return (player === "SCISSORS" || player === "LIZARD") ? "You win!" : "You lose!";
+    }
+    else if (computer === "SCISSORS") {
+        return (player === "ROCK" || player === "SPOCK") ? "You win!" : "You lose!";
+    }
+    else if (computer === "SPOCK") {
+        return (player === "LIZARD" || player === "PAPER") ? "You win!" : "You lose!";
+    }
+    else if (computer === "LIZARD") {
+        return (player === "SCISSORS" || player === "ROCK") ? "You win!" : "You lose!";
+    }
+}
+
+/* score function */
