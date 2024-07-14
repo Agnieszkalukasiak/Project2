53d9e81b06e2e91a4e0891f51a1a9db9b57dbb9d
Agnieszka Lukasiak <lukasiak@me.com>
2024-07-14 09:31:01 +0000

    add variables to js

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+/* declaring variables, linking to dom elements*/
+
+const playerObj= document.qetElementById("player");
+const computerObj= document.qetElementById("computer");
+const resultObj= document.qetElementById("result");
+const buttonObj= document.qetElementById("button");
+const computerScoreObj=ocument.qetElementById("scoreC");
+const playerScoreObj=ocument.qetElementById("scoreP");
+let player;
+let computer;
+let result;
+/* */
+let playerScore = 0;
+let computerScore = 0;
